9945e88ba5e8993d0f3be9f82b459c433ed0cb19
Ran <huangran.alex@gmail.com>
2023-08-28 14:22:50 +0800

    Update sync_mult_prs.sh

--- sync_mult_prs.sh
+++ sync_mult_prs.sh
@@ -4,12 +4,10 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 
-./sync.sh preview/pingcap/docs/{PR_NUMBER_1}
-./sync.sh preview/pingcap/docs/{PR_NUMBER_2}
-./sync.sh preview/pingcap/docs-cn/{PR_NUMBER_3}
-./sync.sh preview/pingcap/docs-cn/{PR_NUMBER_4}
-rsync -av markdown-pages/zh/tidb/master/ markdown-pages/zh/tidb/{release-x.y}/
-rsync -av markdown-pages/en/tidb/master/ markdown-pages/en/tidb/{release-x.y}/
+./sync.sh preview/pingcap/docs-cn/14828
+./sync.sh preview/pingcap/docs/14575
+rsync -av markdown-pages/zh/tidb/master/ markdown-pages/zh/tidb/master/
+rsync -av markdown-pages/en/tidb/master/ markdown-pages/en/tidb/master/
 
 git add .
 git commit -m "update {release-x.y}" || echo "No changes to commit"
